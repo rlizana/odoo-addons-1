30a01c44bdd4edd9aacf25b34347f78bbfb3c3c3
dani-ds <danielcampos@avanzosc.com>
2012-09-25 13:16:59 +0200

    [ADD] Repairdest_mod

---
+++ avanzosc_repairdest_mod/__openerp__.py
@@ -0,0 +1,43 @@
+
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2008 Daniel (Avanzosc) <danielcampos@avanzosc>
+#    03/11/2011
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    "name" : "Avanzosc Repair Destination Mod",
+    "version" : "1.0",
+    "description": """ 
+                This module modify:
+                 - Product repaired destination is where it came from.
+                    """,
+    "author": "AvanzOSC",
+    "website" : "http://www.avanzosc.com",
+    "depends" : ["base","mrp_repair"],
+    "category" : "Generic Modules",
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : [
+                    ],
+    "active" : False,
+    "installable" : True
+    
+}
---
+++ avanzosc_repairdest_mod/repair_dest_mod.py
@@ -0,0 +1,39 @@
+
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution    
+#    Copyright (C) 2008-2012 Daniel (AvanzOSC). All Rights Reserved
+#    
+#    
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+from osv import fields,osv
+from tools.translate import _
+
+class mrp_repair(osv.osv): 
+
+    _inherit = 'mrp.repair'
+    
+    def onchange_move_id(self, cr, uid, ids, prod_id=False, move_id=False, context=False):
+        data = super(mrp_repair, self).onchange_move_id(cr, uid, ids, prod_id, move_id)
+        if data ['value']['location_dest_id'] :
+            move = self.pool.get('stock.move').browse(cr, uid, move_id)
+            data ['value']['location_dest_id'] = move.location_id.id
+        return data
+    
+mrp_repair()
+
