18f37874a696ab52569d4b36a515bf0029cead1e
dani-ds <danielcampos@avanzosc.com>
2013-06-11 09:21:44 +0200

    [ADD] avanzosc_partner_address_zone

---
+++ avanzosc_partner_address_zone/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Avanzosc - Avanced Open Source Consulting
+#    Copyright (C) 2011 - 2013 Avanzosc <http://www.avanzosc.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+{
+    "name": "Avanzosc Partner Address Zone",
+    "version": "1.0",
+    "depends": ["base","stock"],
+    "author": "AvanzOSC",
+    "category": "Custom Modules",
+    "description": """
+        Avanzosc Custom Modules. Create master table ZONES. Include many2one in address to zones.
+        Include filter and grouping area customers. Show on tree, filter and grouping of albaranes too.
+
+    """,
+    "init_xml": [],
+    'update_xml': ['res_partner_address_zone_view.xml',
+                   'res_partner_address_ext_view.xml',
+                   'stock_picking_ext_view.xml',
+                   ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+#    'certificate': 'certificate',
+}
---
+++ avanzosc_partner_address_zone/res_partner_address_zone.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Avanzosc - Avanced Open Source Consulting
+#    Copyright (C) 2011 - 2013 Avanzosc <http://www.avanzosc.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+from osv import osv
+from osv import fields
+import decimal_precision as dp
+from tools.translate import _
+
+class res_partner_address_zone(osv.osv):
+    _name = 'res.partner.address.zone'
+    _description = 'Parner Address Zone'
+    
+     
+    _columns = {# Descripcion
+                'name':fields.char('Name', size=64, required=True),
+                }
+
+res_partner_address_zone()
