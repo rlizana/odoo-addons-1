1bf57ccc451e24c4c7b68e5556126a22102a7174
oihanecruce <>
2013-06-17 13:03:50 +0200

    [MOD] Tree_grid added in stock.picking

--- tree_grid/__openerp__.py
+++ tree_grid/__openerp__.py
@@ -26,7 +26,7 @@
     "version" : "1.1",
     "author" : "Ting!, Avanzosc",
     "category" : "Generic",
-    "depends" : ["base","sale"],
+    "depends" : ["base","sale","stock"],
     "init_xml" : [],
     "description": """
         Editable tree view with virtual and real stocks for sales orders.
