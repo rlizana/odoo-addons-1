6e88df8cf746c71f690aae375b3d28fb72c52735
dani-ds <danielcampos@avanzosc.com>
2013-07-03 13:42:37 +0200

    [ADD] avanzosc_product_change_unit_of_mesure

---
+++ avanzosc_product_change_unit_of_mesure/__openerp__.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Avanzosc - Avanced Open Source Consulting
+#    Copyright (C) 2011 - 2012 Avanzosc <http://www.avanzosc.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+{
+    "name": "Avanzosc Product Change Unit Of Mesure",
+    "version": "1.0",
+    "depends": ["product"],
+    "author": "Avanzosc",
+    "category": "Custom Modules",
+    "description": """
+        This module allows you to change the unit of measure of the product.
+    """,
+    "init_xml": [],
+    'update_xml': [],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
