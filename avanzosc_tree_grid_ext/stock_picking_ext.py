1939835434b346d2de3578a8dc03e6ce1b58f160
Oihane <oihanecruce@gmail.com>
2014-03-13 18:26:36 +0100

    [MOD] Fixed tree_grid incompatibility with sale_payment

--- avanzosc_tree_grid_ext/stock_picking_ext.py
+++ avanzosc_tree_grid_ext/stock_picking_ext.py
@@ -25,7 +25,6 @@ class stock_picking(osv.osv):
     
     _inherit = 'stock.picking'
     
-    
     def _prepare_invoice_line(self, cr, uid, group, picking, move_line, invoice_id,
         invoice_vals, context=None):
         """ Builds the dict containing the values for the invoice line
@@ -49,48 +48,5 @@ class stock_picking(osv.osv):
         res['price_unit'] = move_line.sale_line_id.price_unit
             
         return res
-        
-#        if group:
-#            name = (picking.name or '') + '-' + move_line.name
-#        else:
-#            name = move_line.name
-#        origin = move_line.picking_id.name or ''
-#        if move_line.picking_id.origin:
-#            origin += ':' + move_line.picking_id.origin
-#
-#        if invoice_vals['type'] in ('out_invoice', 'out_refund'):
-#            account_id = move_line.product_id.product_tmpl_id.\
-#                    property_account_income.id
-#            if not account_id:
-#                account_id = move_line.product_id.categ_id.\
-#                        property_account_income_categ.id
-#        else:
-#            account_id = move_line.product_id.product_tmpl_id.\
-#                    property_account_expense.id
-#            if not account_id:
-#                account_id = move_line.product_id.categ_id.\
-#                        property_account_expense_categ.id
-#        if invoice_vals['fiscal_position']:
-#            fp_obj = self.pool.get('account.fiscal.position')
-#            fiscal_position = fp_obj.browse(cr, uid, invoice_vals['fiscal_position'], context=context)
-#            account_id = fp_obj.map_account(cr, uid, fiscal_position, account_id)
-#        # set UoS if it's a sale and the picking doesn't have one
-#        uos_id = move_line.product_uos and move_line.product_uos.id or False
-#        if not uos_id and invoice_vals['type'] in ('out_invoice', 'out_refund'):
-#            uos_id = move_line.product_uom.id
-
-#        return {
-#            'name': name,
-#            'origin': origin,
-#            'invoice_id': invoice_id,
-#            'uos_id': uos_id,
-#            'product_id': move_line.product_id.id,
-#            'account_id': account_id,
-#            'price_unit': self._get_price_unit_invoice(cr, uid, move_line, invoice_vals['type']),
-#            'discount': self._get_discount_invoice(cr, uid, move_line),
-#            'quantity': move_line.product_uos_qty or move_line.product_qty,
-#            'invoice_line_tax_id': [(6, 0, self._get_taxes_invoice(cr, uid, move_line, invoice_vals['type']))],
-#            'account_analytic_id': self._get_account_analytic_invoice(cr, uid, picking, move_line),
-#        }
     
 stock_picking()
